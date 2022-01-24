265c7da73938289bad62abf958d3ca2eca43c9f7
asivolap <asivolap@openwaygroup.com>
2022-01-24 13:09:32 +0300

    added func for getting area id added filter for search: City, Period

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+import os
 import requests
+from dotenv import load_dotenv
 
 
-def main():
-    hh_link = "https://api.hh.ru/vacancies/"
+def get_area_id(city):
+    area_link = "https://api.hh.ru/suggests/areas/"
     params = {
-        "text": "программист"
+        "text": city
     }
-    response = requests.get(hh_link, params)
+    response = requests.get(area_link, params)
+    response.raise_for_status()
+    return response.json().get("items")[0].get("id")
+
+
+def find_job(city_id):
+    vacancies_link = "https://api.hh.ru/vacancies/"
+    params = {
+        "text": "программист",
+        "area": city_id,
+        "period": 30,
+        "per_page": 100
+    }
+    response = requests.get(vacancies_link, params)
     response.raise_for_status()
     for i in response.json().get("items"):
         print(i.get("name"))
+    print(response.json().get("found"))
+
+
+def main():
+    load_dotenv()
+    city = os.getenv("CITY")
+    city_id = get_area_id(city)
+    find_job(city_id)
 
 
 if __name__ == "__main__":
