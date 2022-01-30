816d84ff99f9462753d343b925132c3fb55da825
Arthur <aptypic@gmail.com>
2022-01-30 03:41:15 +0300

    deleted find_job,  changed compare_demand_pl, output 3 conclusions; vacancies_found, vacancies_processed, average_salary

--- main.py
+++ main.py
@@ -13,54 +13,55 @@ def get_area_id(city):
     return response.json().get("items")[0].get("id")
 
 
-def find_job(city_id, pl):
-    vacancies_link = "https://api.hh.ru/vacancies/"
-    params = {
-        "text": f"{pl}",
-        "area": city_id,
-        "period": 30,
-        "per_page": 100
-    }
-    response = requests.get(vacancies_link, params)
-    response.raise_for_status()
-    return response.json().get("found")
-
-
-def compare_demand_pl(city_id, popular_pl, pl_statistic):
+def compare_demand_pl(city_id, popular_pl):
+    pl_statistic = []
     for pl in popular_pl:
-        pl_statistic[pl] = find_job(city_id, pl)
-    print(pl_statistic)
+        info_results = predict_rub_salary(city_id, pl)
+        pl_statistic.append({
+            f"{pl}": {
+                "vacancies_found": info_results[2],
+                "vacancies_processed": info_results[1],
+                "average_salary": info_results[0]
+                    }})
+    return pl_statistic
 
 
 def predict_rub_salary(city_id, pl):
+    vacancies_page_number = 1
+    vacancies_page = 0
+    average_results = []
     vacancies_link = "https://api.hh.ru/vacancies/"
     params = {
         "text": f"{pl}",
         "area": city_id,
         "period": 30,
-        "per_page": 100
+        "per_page": 100,
+        "page": vacancies_page
     }
-    response = requests.get(vacancies_link, params)
-    response.raise_for_status()
-    for i in (response.json().get("items")):
-        if i.get("salary") is not None and i.get("salary").get("currency") == "RUR":
-            min_salary = i.get("salary").get("from")
-            max_salary = i.get("salary").get("to")
-            if min_salary is not None and max_salary is not None:
-                print(round((min_salary+max_salary)/2), i.get("salary").get("currency"))
-            elif min_salary is not None:
-                print(round(min_salary*1.2), i.get("salary").get("currency"))
-            else:
-                print(round(max_salary*0.8), i.get("salary").get("currency"))
+    while vacancies_page < vacancies_page_number:
+        response = requests.get(vacancies_link, params)
+        response.raise_for_status()
+        vacancies_page_number = response.json().get("pages")
+        vacancies_page += 1
+        for i in (response.json().get("items")):
+            if i.get("salary") is not None and i.get("salary").get("currency") == "RUR":
+                min_salary = i.get("salary").get("from")
+                max_salary = i.get("salary").get("to")
+                if min_salary is not None and max_salary is not None:
+                    average_results.append(round((min_salary + max_salary) / 2))
+                elif min_salary is not None:
+                    average_results.append(round(min_salary * 1.2))
+                else:
+                    average_results.append(round(max_salary * 0.8))
+    return round(sum(average_results) / len(average_results)), len(average_results), response.json().get("found")
 
 
 def main():
     popular_pl = ["Python", "Java", "Javascript", "Golang", "C++", "Rust", "php"]
-    pl_statistic = {}
     load_dotenv()
     city_id = get_area_id(os.getenv("CITY"))
-    # compare_demand_pl(city_id, popular_pl, pl_statistic)
-    predict_rub_salary(city_id, popular_pl[0])
+    print(compare_demand_pl(city_id, popular_pl))
+    # print(predict_rub_salary(city_id, popular_pl[0]))
 
 
 if __name__ == "__main__":
